329c945346c73b0e798830239b9f9e92bc979185
Lilian <liliansungg@gmail.com>
2022-05-16 14:39:44 +0200

    2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,10 +12,14 @@ const Persons = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -44,9 +48,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilterName(event.target.value)
+  }
+  
+  const peopleToShow = filterName === '' ?
+  persons : persons.filter(person => person.name === filterName)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={setFilterName}>
+        <div>
+          filter shown with <input value={filterName} onChange={handleFilterChange}/>
+        </div>
+      </form>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/>
@@ -61,7 +80,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={peopleToShow}/>
 
     </div>
   )
