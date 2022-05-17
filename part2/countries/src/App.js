81600d1d2acd4eede6ca1cabb588f19eceb1ec85
Lilian <liliansungg@gmail.com>
2022-05-17 12:05:25 +0200

    2.14 Data for countries, step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,31 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({country}) => {
+const Country = ({country, apiKey}) => {
+  const [weather, setWeather] = useState({
+    "main":{"temp":0},
+    "wind":{"speed":0},
+    "weather":[{"icon":"01d"}]
+  })
+
+  console.log(country);
+
+  let lat = country.latlng[0]
+  let lon = country.latlng[1]
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+      .then(response => {
+        console.log('promise fulfilled')
+        console.log(response.data)
+        setWeather(response.data)
+      })
+  }, [])
+
+  console.log(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+
   return(
     <div>
       <h1>{country.name.common}</h1>
@@ -16,11 +40,16 @@ const Country = ({country}) => {
         )}
       </ul>
       <img src={country.flags.png} alt={country.name.common + ' flag'}></img>
+
+      <h1>Weather in {country.capital[0]}</h1>
+      <div>temperature {weather.main.temp} Celcius</div>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+      <div>wind {weather.wind.speed} m/s</div>
     </div>
   )
 }
 
-const CountryName = ({country}) => {
+const CountryName = ({country, apiKey}) => {
   const [showCountry, setShowCountry] = useState(false)
 
   return(
@@ -30,12 +59,12 @@ const CountryName = ({country}) => {
         {showCountry? 'hide' : 'show'}
       </button>
       {showCountry? 
-      <Country country={country}/>:''}
+      <Country country={country} apiKey={apiKey}/>:''}
     </div>
   )
 }
 
-const Countries = ({countries}) => {
+const Countries = ({countries, apiKey}) => {
   //console.log(countries[0].name.common)
   if (countries.length > 10) {
     return (
@@ -44,13 +73,13 @@ const Countries = ({countries}) => {
   } else if (countries.length > 1) {
     return (
       <div>
-        {countries.map(country => <CountryName country={country} />)}
+        {countries.map(country => <CountryName key={country.name.common} country={country} apiKey={apiKey}/>)}
       </div>
     )
   } else if (countries.length === 1) {
     //console.log(countries[0])
     return (
-      <Country country={countries[0]}/>
+      <Country country={countries[0]} apiKey={apiKey}/>
     )
   }
 }
@@ -58,6 +87,7 @@ const Countries = ({countries}) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const api_key = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
     //console.log('effect')
@@ -85,7 +115,7 @@ const App = () => {
           find countries <input value={filter} onChange={handleFilterChange}/>
         </div>
       </form>
-      <Countries countries={filteredCountries}/>
+      <Countries countries={filteredCountries} apiKey={api_key}/>
     </div>
 
   )
