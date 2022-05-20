99e42255369ec3436b0f8ca7eb0e8ccb0f379aa7
Lilian <liliansungg@gmail.com>
2022-05-20 14:22:54 +0200

    3.16: Phonebook database, step4

--- part3/index.js
+++ part3/index.js
@@ -1,11 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
 const app = express()
+const Person = require('./models/person')
+const { response } = require('express')
+const { default: mongoose } = require('mongoose')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
+//app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 morgan.token('content', (request, response) => {
@@ -41,18 +47,21 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    } 
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -63,13 +72,10 @@ app.get('/info', (request, response) => {
     )
 })
 
-const generateId = () => Math.floor(Math.random() * 200)
-
-
+//const generateId = () => Math.floor(Math.random() * 200)
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
 
     const checkName = persons.some(person => person.name === body.name)
     //console.log(checkName)
@@ -84,23 +90,38 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
+        //id: generateId()
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
     }
 
-    persons = persons.concat(person)
-    response.json(person)
-})
+    next(error)
+}
 
-app.delete('/api/persons/:id', (request,response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
