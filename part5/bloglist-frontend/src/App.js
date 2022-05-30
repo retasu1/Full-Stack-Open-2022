9112a9c94d79207398a521b639ab1e7de14810b4
Lilian <liliansungg@gmail.com>
2022-05-30 13:38:13 +0200

    5.2: bloglist frontend, step2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -22,12 +31,25 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('Wrong credentials')
     }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    console.log(`logging ${user.name} out`)
+
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -64,6 +86,9 @@ const App = () => {
       <h2>blogs</h2>
       <p>
         {user.name} logged in
+        <button onClick={handleLogout}>
+          logout
+        </button>
       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
